5b04be2183871842dc04274e0fb030c584e2ad6b
Ahmed-Nezar <ahmednezarrr@gmail.com>
2024-04-13 14:38:49 +0200

    feat: adjusting Passenger-FlightSchedule-Reservation Classes

--- Airlines/src/main/java/org/example/airlines/FlightSchedule.java
+++ Airlines/src/main/java/org/example/airlines/FlightSchedule.java
@@ -1,5 +1,111 @@
 package org.example.airlines;
 
+import java.util.Vector;
+
 public class FlightSchedule {
+    private Vector<Flight> flights;
+
+    public FlightSchedule() {
+        this.flights = new Vector<Flight>();
+    }
+
+    public void addFlight(Flight flight) {
+        flights.add(flight);
+    }
+
+    public void removeFlight(String flightId) {
+        for (Flight flight : flights) {
+            if (flight.getFlightId().equals(flightId)) {
+                flights.remove(flight);
+                break;
+            }
+        }
+    }
+
+    public Flight getFlight(String flightId) {
+        for (Flight flight : flights) {
+            if (flight.getFlightId().equals(flightId)) {
+                return flight;
+            }
+        }
+        return null;
+    }
+
+    // modify flight price
+    public void modifyFlightPrice(String flightId, String price) {
+        for (Flight flight : flights) {
+            if (flight.getFlightId().equals(flightId)) {
+                flight.setPrice(price);
+                break;
+            }
+        }
+    }
+
+    // modify flight status
+
+    public void modifyFlightStatus(String flightId, String status) {
+        for (Flight flight : flights) {
+            if (flight.getFlightId().equals(flightId)) {
+                flight.setStatus(status);
+                break;
+            }
+        }
+    }
+
+    // modify flight duration
+    public void modifyFlightDuration(String flightId, String duration) {
+        for (Flight flight : flights) {
+            if (flight.getFlightId().equals(flightId)) {
+                flight.setDuration(duration);
+                break;
+            }
+        }
+    }
+
+    // modify flight arrival time
+    public void modifyFlightArrivalTime(String flightId, String arrivalTime) {
+        for (Flight flight : flights) {
+            if (flight.getFlightId().equals(flightId)) {
+                flight.setArrivalTime(arrivalTime);
+                break;
+            }
+        }
+    }
+
+    // modify flight departure time
+    public void modifyFlightDepartureTime(String flightId, String departureTime) {
+        for (Flight flight : flights) {
+            if (flight.getFlightId().equals(flightId)) {
+                flight.setDepartureTime(departureTime);
+                break;
+            }
+        }
+    }
+
+
+    public void displayFlights() {
+        for (Flight flight : flights) {
+            flight.displayInfo();
+        }
+    }
+    public Flight searchFlight(String flightId) {
+        for (Flight flight : flights) {
+            if (flight.getFlightId().equals(flightId)) {
+                flight.displayInfo();
+                return flight;
+            }
+        }
+        return null;
+    }
+    public Flight searchFlight(String source, String destination) {
+        for (Flight flight : flights) {
+            if (flight.getSource().equals(source) && flight.getDestination().equals(destination)) {
+                flight.displayInfo();
+                return flight;
+            }
+        }
+        return null;
+    }
+
 
 }
--- Airlines/src/main/java/org/example/airlines/Reservation.java
+++ Airlines/src/main/java/org/example/airlines/Reservation.java
@@ -2,23 +2,17 @@ package org.example.airlines;
 
 public class Reservation {
     private String reservationId;
-    private String flightId;
-    private String userId;
     private String seatNumber;
     private String status;
 
     public Reservation() {
         this.reservationId = "";
-        this.flightId = "";
-        this.userId = "";
         this.seatNumber = "";
         this.status = "";
     }
 
     public Reservation(String reservationId, String flightId, String userId, String seatNumber, String status) {
         this.reservationId = reservationId;
-        this.flightId = flightId;
-        this.userId = userId;
         this.seatNumber = seatNumber;
         this.status = status;
     }
@@ -27,13 +21,6 @@ public class Reservation {
         this.reservationId = reservationId;
     }
 
-    public void setFlightId(String flightId) {
-        this.flightId = flightId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
 
     public void setSeatNumber(String seatNumber) {
         this.seatNumber = seatNumber;
@@ -41,18 +28,6 @@ public class Reservation {
 
     public void setStatus(String status) {
         this.status = status;
-    }
-
-    public String getReservationId() {
-        return reservationId;
-    }
-
-    public String getFlightId() {
-        return flightId;
-    }
-
-    public String getUserId() {
-        return userId;
     }
 
     public String getSeatNumber() {
